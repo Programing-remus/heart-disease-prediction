11c9ad7629e33bad78edeea6ad1a6e7ac0c6cbb9
Leonor Sinval Fernandes <139269936+Programing-remus@users.noreply.github.com>
2025-08-23 12:02:22 +0000

    Version 2 - tests

--- project.py
+++ project.py
@@ -3,5 +3,14 @@
 def main():
     print("Let's get this project started")
 
+def function1():
+    ...
+
+def function2():
+    ...
+
+def function3():
+    ...
+
 if __name__ == "__main__":
     main()
