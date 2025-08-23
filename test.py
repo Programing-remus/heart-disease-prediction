2d0731ddd56af698fe3616a13694ff06c3aa9b6a
Leonor Sinval Fernandes <139269936+Programing-remus@users.noreply.github.com>
2025-08-23 12:05:05 +0000

    Version4 test

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 
 # Test pandas functionalities on the csv file
+# https://www.kaggle.com/datasets/johnsmith88/heart-disease-dataset?resource=download - check this to learn about manipulating heart data
 
 print("Test pandas on the heart.csv file")
