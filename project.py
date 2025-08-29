ac316aa757240e7d2eee57b5f162ea450aa5fae5
Leonor S. F. <leonor.sinval.fernandes@gmail.com>
2025-08-29 22:35:08 +0100

    Functions for Logistic Regression Model implementation

--- project.py
+++ project.py
@@ -1,16 +1,69 @@
-# Heart disease prediction program based on cardiac and colesterol data
+# Heart disease probability prediction program based on the input of patient cardiac data
+
+import numpy as np
+import pandas as pd
+
+# IMPORTANT VARIABLES
+# m - number of iterations in x
+# model - updated model (n x 1)
+# x - numpy matrix containing the dataset data (m x n) 
+# y - binary result associated with each instance of x (m x 1)
+# alpha - training rate (starting test with default 0.01)
+# grad - gradient of the cross entropy loss function
+# heart_disease_predict - logistic regression model for the prediction of heart disease risk
+
+def sigmoid(z):
+    return 1/(1+np.exp(-z))
+
+# Calculate the gradient of the cross entropy loss function
+def get_gradient(model,x,y):
+    m = np.shape(x)[0] # m is the number of instances, in this case, the lines of the data frame that originated x
+    grad = (np.transpose(x) @ (sigmoid(x @ model)-y))/m
+    return grad
+
+# Calculates the cross entropy loss function of a given model, trained with x vector and y vector for labels
+def entropy_loss(model, x, y):
+    m = np.shape(x)[0]
+    J = []
+    for i in range(m):
+        j = y[i]*np.log(sigmoid(x[i]*model[i]))+(1-y[i])*np.log(1-sigmoid(x[i]*model[i]))
+        J.append(j)
+    return -sum(J)/m
+        
+# Returns the difference between the cross entropy loss function of the previous model and the current 
+def dif_cost(prev_model,model,x,y):
+    return entropy_loss(prev_model,x,y) - entropy_loss(model,x,y)
+
+# Model training - implementing the gradient descent method from scratch to train the model
+def log_reg_model(x,y,alpha=0.1,min=10^-4):
+    m = np.shape(x)[0]
+    x_int = np.concatenate((np.ones(m),x), axis=1) # Needed because of the bias (x0)
+    model = np.zeros((np.shape((x_int,1)),1)) # Initialize the model as a null matrix with n+1 columns
+
+    # Iterates in a large range and breaks when the difference between the costs of the previous model and the updated one is less than min 
+    for _ in range(10000):
+        prev_model = model
+        model -= alpha*get_gradient(model,x_int,y)
+        if dif_cost(prev_model, model, x_int, y) < min:
+            break
+    return model
+
+# Returns data to use for training and separates data from labels, using pandas
+def select_train(df,size=10):
+    ...
+    
+# Implementation of a class that allows manipulation of patient data:
+# Handle input
+class Patient():
+    ...
 
 def main():
-    print("Let's get this project started")
+    data = pd.read_csv("heart.csv", sep=",")
+    X,Y = select_train(data)
+    heart_disease_predict = log_reg_model(X,Y)
+    # test this with pytest. It would be cool to test accuracy in the future
 
-def function1():
-    ...
 
-def function2():
-    ...
-
-def function3():
-    ...
 
 if __name__ == "__main__":
     main()
---
+++ test_project.py
@@ -0,0 +1,1 @@
+from project import sigmoid, get_gradient, entropy_loss, dif_cost, log_reg_model, select_train
